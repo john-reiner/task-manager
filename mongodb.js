092335eb57c9b36e744168ec362b5d5623536fc8
john-reiner <johnreiner1990@gmail.com>
2020-08-31 16:15:58 -0400

    added insterMany function to db

--- mongodb.js
+++ mongodb.js
@@ -12,8 +12,55 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     }
 
     const db = client.db(databaseName)
-    db.collection('users').insertOne({
-        name: 'John', 
-        age: 30
+    // db.collection('users').insertOne({
+    //     name: 'John', 
+    //     age: 30
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert user')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Jess',
+    //         age: 29
+    //     },
+    //     {
+    //         name: 'Dave',
+    //         age: 31
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert documents!!')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    db.collection('users').insertMany([
+        {
+            description: 'Apply to 3 jobs by Wednesday.', 
+            completed: false
+        },
+        {
+            description: 'Wake up at 4am every day this week.', 
+            completed: false
+        },
+        {
+            description: 'Go to the gym 5 out of 7 days this week.', 
+            completed: false
+        }
+    ], (error, result) => {
+        if (error) {
+            return console.log('cannot get docs!')
+        }
+
+        console.log(result.ops)
+
     })
+
+
 })
