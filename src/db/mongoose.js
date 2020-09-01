06185e22cde02ad10347e94404aae7117647f20d
john-reiner <johnreiner1990@gmail.com>
2020-09-01 15:38:49 -0400

    added a user model to db and created new db

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,3 +4,19 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser : true,
     useCreateIndex: true,
 })
+
+const User = mongoose.model('User', {
+    name: {
+        type: String
+    },
+    age : {
+        type: Number
+    }
+})
+
+const me = new User({
+    name: 'John',
+    age: 'thirty'
+})
+
+me.save().then(response => console.log(me)).catch(error => console.log(error))
