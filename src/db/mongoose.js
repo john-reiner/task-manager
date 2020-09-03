f57c622c44155517f4e5b1e7e78b6a29921551a3
john-reiner <johnreiner1990@gmail.com>
2020-09-03 13:58:56 -0400

    sanitization

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
+
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser : true,
@@ -7,16 +9,52 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type : String,
+        required: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("not a valid email")
+            }
+        },
+        trim : true
     },
     age : {
-        type: Number
+        type: Number,
+        default: 0, 
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number.')
+            }
+        }
     }
 })
 
 const me = new User({
-    name: 'John',
-    age: 'thirty'
+    name: '  John   ',
+
+    email : 'john@gmail.com'
 })
 
 me.save().then(response => console.log(me)).catch(error => console.log(error))
+
+
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String,
+        required: true
+    },
+    completed: {
+        type: Boolean
+    }
+})
+
+const task1 = new Task({description: 'Go to the gym.', completed: false})
+
+task1.save().then(Response => console.log(task1)).catch(error => console.log(error))
