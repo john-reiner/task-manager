a6bcdbcc1db5a50dacae0c37a91d10830865fa4a
john-reiner <johnreiner1990@gmail.com>
2020-09-08 19:15:13 -0400

    added get requests for Users and Tasks

--- src/index.js
+++ src/index.js
@@ -14,11 +14,53 @@ app.post('/users', (req, res) => {
     user.save().then(() => res.send(user)).catch(e => res.status(400).send(e))
 })
 
+app.get('/users', (req,res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+
+    User.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body)
-    task.save().then(() => res.send(task)).catch(e => res.status(400).send(e))
+    task.save().then(() => res.status(201).send(task)).catch(e => res.status(400).send(e))
+})
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((e) => res.send(500))
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+
+    Task.findById(_id).then((task) => {
+        if (task === undefined) {
+            return res.status(404).send()
+
+        }
+        res.send(task)
+    }).catch((e) => {
+        res.status(500).send()
+    })
 })
 
 app.listen(port, () => {
     console.log('Server is on port : ', port)
-})
+}) 
