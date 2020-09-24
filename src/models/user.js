72b66043e4be1efcc97eb590ea1a27e73bc7c2fb
john-reiner <johnreiner1990@gmail.com>
2020-09-23 21:49:53 -0400

    added and set middleware for user

--- src/models/user.js
+++ src/models/user.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 
-
-const User = mongoose.model('User', {
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -44,5 +43,15 @@ const User = mongoose.model('User', {
         }
     }
 })
+
+userSchema.pre('save', async function (next) {
+    const user = this
+
+    console.log('just before saving')
+
+    next()
+})
+
+const User = mongoose.model('User', userSchema)
 
 module.exports = User
