8f388985bf4bcddcd1e35fb7b3b2d1a645239954
john-reiner <johnreiner1990@gmail.com>
2020-09-28 20:47:05 -0400

    created a token for a new user

--- src/routers/user.js
+++ src/routers/user.js
@@ -4,10 +4,10 @@ const router = new express.Router()
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
-
+    const token = await user.generateAuthToken()
     try {
         await user.save()
-        res.status(201).send(user)
+        res.status(201).send({ user, token})
     } catch (e) {
         res.status(400).send(e)
     }
