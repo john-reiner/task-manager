2dd125559b1c552cbe637e947bc534d148a329bb
john-reiner <johnreiner1990@gmail.com>
2020-10-20 17:14:10 -0400

    added if statment to middleware

--- src/index.js
+++ src/index.js
@@ -7,10 +7,15 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use((req, res, next) => {
-    console.log(req.method, req.path)
-    next()
-})
+// app.use((req, res, next) => {
+//     if (req.method === "GET") {
+//         res.send('GET Requests are disabled')
+//     } else {
+//         next()
+//     }
+// })
+
+
 
 app.use(express.json())
 app.use(userRouter)
